7b930ccf98057bd67b48b5134b52e8bd1e5176f9
Pedro Tammela <pctammela@gmail.com>
2018-12-06 19:19:18 -0200

    add gc regression test

---
+++ tests/gc.lua
@@ -0,0 +1,33 @@
+--
+-- Lua binding for LLVM C API.
+-- Copyright (C) 2018 Matheus Ambrozio, Pedro Tammela, Renan Almeida.
+--
+-- This file is part of lua-llvm-binding.
+--
+-- lua-llvm-binding is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 2 of the License, or
+-- (at your option) any later version.
+--
+-- lua-llvm-binding is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with lua-llvm-binding. If not, see <http://www.gnu.org/licenses/>.
+--
+--
+
+-- a LLVM module must not be disposed until we are done with it
+
+local llb = require'llb'
+local testing = require'testing'
+
+testing.header("GC Regression")
+
+for i = 1, 2^8 do
+    llb.load_ir("aux/simple.ll")['main']:domtree()
+end
+
+testing.ok()
