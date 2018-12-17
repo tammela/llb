81df7386573bfa3f8eef8f684fec47083b34ab02
Renan <renan.061@gmail.com>
2018-12-17 20:08:03 -0200

    Some refactoring and added phis location to fn:prunedssa()

--- src/function.lua
+++ src/function.lua
@@ -19,34 +19,9 @@
 --
 
 local set = require "set"
+local bbgraph = require "bbgraph"
 
 local fn = {}
-
------------------------------------------------------
---
---  auxiliary
---
------------------------------------------------------
-
-local graph = {}
-
-function graph.new()
-    local t = {}
-    setmetatable(t, graph)
-    return t
-end
-
-function graph:__tostring()
-    local nodes = {}
-    for _, node in ipairs(self) do
-        local t = {}
-        table.insert(t, "label: " .. tostring(node.ref))
-        table.insert(t, "successors: " .. tostring(node.successors))
-        table.insert(t, "predecessors: " .. tostring(node.predecessors))
-        table.insert(nodes, table.concat(t, "\n"))
-    end
-    return "---\n" .. table.concat(nodes, "\n---\n") .. "\n---"
-end
 
 -----------------------------------------------------
 --
@@ -55,237 +30,69 @@ end
 -----------------------------------------------------
 
 -- computes the predecessors-sucessors graph of a function
-function fn:bbgraph()
-    local bbs = self:basic_blocks()
-    local nodes = graph.new()
+function fn:bbgraph(bbs)
+    local bbs = bbs or self:basic_blocks()
+    return bbgraph.new(bbs)
+end
+
+-- TODO: local?
+function fn:map_instructions(bbgraph)
+    local instructions = {}
     local auxmap = {}
 
-    for i, bb in ipairs(bbs) do
-        nodes[i] = {
-            ref = bb,
-            successors = set.new(),
-            predecessors = set.new()
-        }
-        auxmap[bb:pointer()] = nodes[i]
-    end
-
-    for _, node in ipairs(nodes) do
-        for _, s in ipairs(node.ref:successors()) do
-            local successor = auxmap[s]
-            node.successors:add(successor)
-            successor.predecessors:add(node)
+    -- getting all instructions
+    local i = 1
+    for _, bb in ipairs(bbgraph) do
+        for _, inst in ipairs(bb.ref:instructions()) do
+            instructions[inst] = {
+                id = i,
+                bb = bb,
+                ref = inst,
+                usages = {}
+            }
+            auxmap[inst:pointer()] = instructions[inst]
+            i = i + 1
         end
     end
 
-    return nodes
-end
-
--- computes the dominators of basic blocks in a function
-function fn:domtree(bbgraph)
-    local bbgraph = bbgraph or self:bbgraph()
-
-    local all = set.new(table.unpack(bbgraph))
-    local entry = bbgraph[1] -- TODO: is the entry bb always bbgraph[1]?
-    local regular_nodes = all - {entry}
-    local dom = {} -- {node: set<node>}
-
-    dom[entry] = set.new(entry)
-    for n in pairs(regular_nodes) do
-        dom[n] = all
-    end
-
-    repeat
-        local change = false
-        for n in pairs(regular_nodes) do
-            local D = all
-            for p in pairs(n.predecessors) do
-                D = D * dom[p]
-            end
-            D = D + {n}
-            if D ~= dom[n] then
-                change = true
-                dom[n] = D
-            end
+    -- mapping usages.
+    -- where the result of the instruction is used as an argument
+    for _, inst in pairs(instructions) do
+        for _, u in ipairs(inst.ref:usages()) do
+            local usage = auxmap[u]
+            table.insert(inst.usages, usage)
         end
-    until not change
-
-    return dom
-end
-
--- computes the strict dominance tree of a function
-function fn:sdomtree(bbgraph)
-    local bbgraph = bbgraph or self:bbgraph()
-    local sdom = self:domtree(bbgraph)
-    for k, v in pairs(sdom) do
-        v:remove(k)
-    end
-    return sdom
-end
-
--- computes the imediate dominance tree of a function
-function fn:idomtree(bbgraph)
-    local bbgraph = bbgraph or self:bbgraph()
-    local dom = self:domtree(bbgraph)
-
-    local all = set.new(table.unpack(bbgraph))
-    local entry = bbgraph[1] -- TODO: is the entry bb always bbgraph[1]?
-    local regular_nodes = all - {entry}
-    local idom = {}
-
-    for n in pairs(all) do
-        idom[n] = dom[n] - {n}
     end
 
-    for n in pairs(regular_nodes) do
-        for s in pairs(idom[n]) do
-            for t in pairs(idom[n] - {s}) do
-                if idom[s]:contains(t) then
-                    idom[n] = idom[n] - {t}
+    return instructions
+end
+
+-- puts the IR in pruned SSA form
+-- removes and replaces useless alloca/store/load instructions
+function fn:prunedssa(bbgraph)
+    local bbgraph = bbgraph or self:bbgraph()
+    local instructions = self:map_instructions(bbgraph)
+
+    local phis = {}
+
+    -- for each "alloca" variable "x" 
+    for _, instruction in pairs(instructions) do
+        if instruction.ref:is_alloca() then
+            -- if S is the set of nodes that assign to variable "x"
+            local s = set.new()
+            for _, usage in pairs(instruction.usages) do
+                if usage.ref:is_store() then
+                    s:add(usage.bb)
                 end
             end
+            -- DF+(S) is the set of nodes that need phi-functions for "x"
+            phis[instruction] = bbgraph:dfplus(s)
         end
     end
 
-    idom[entry] = nil
+    -- TODO: falta coisa...
 
-    for k, v in pairs(idom) do
-        idom[k] = v:pop()
-        assert(v:is_empty())
-    end
-
-    return idom
+    return phis
 end
-
--- reverse idomtree
-function fn:ridomtree(bbgraph)
-    local bbgraph = bbgraph or self:bbgraph()
-    local idom = self:idomtree(bbgraph)
-    local ridom = {}
-
-    for _, v in ipairs(bbgraph) do
-        ridom[v] = set.new()
-    end
-
-    for k, v in pairs(idom) do
-        ridom[v]:add(k)
-    end
-
-    return ridom
-end
-
--- FIXME: this is the quadratic version
--- computes the dominance frontier of basic blocks in a function
-function fn:df(bbgraph)
-    local bbgraph = bbgraph or self:bbgraph()
-    local dom = self:domtree(bbgraph)
-    local sdom = self:sdomtree(bbgraph)
-
-    local df = {}
-
-    for _, x in ipairs(bbgraph) do
-        df[x] = set.new()
-    end
-
-    -- df(x) = {y | (z E predecessors(y) | x C dom[z]) and (x !C sdom[y])}
-    for _, x in ipairs(bbgraph) do
-        for _, y in ipairs(bbgraph) do
-            for z in pairs(y.predecessors) do
-                if dom[z]:contains(x) and not sdom[y]:contains(x) then
-                    df[x]:add(y)
-                end
-            end
-        end
-    end
-
-    return df
-end
-
--- computes the iterated dominance frontier (DF+) of nodes in S
--- "if S is the set of nodes that assign to variable x then DF+(S) is exactly
--- the set of nodes that need phi-functions for x"
-function fn:dfplus(bbgraph, s)
-    local s = s:copy()
-    s:add(bbgraph[1])
-
-    local bbgraph = bbgraph or self:bbgraph()
-    local df = self:df(bbgraph)
-
-    local function dfunion(s)
-        local dfu = set.new()
-        for x in pairs(s) do
-            dfu = dfu + df[x]
-        end
-        return dfu
-    end
-
-    -- S = bbgraph
-    -- DF(S) = U[x E S] DF(x)
-    local dfs = dfunion(s)
-
-    -- DF[1](S) = DF(S)
-    -- DF[i+1](S) = DF(S U DF[i](S))
-    -- DF+(S) = lim[i to infinity] DF[i](S)
-    local dfp = dfs
-    repeat
-        local change = false
-        local D = dfunion(s + dfp)
-        if D ~= dfp then
-            dfp = D
-            change = true
-        end
-    until not change
-
-    return dfp
-end
-
--- function fn:map_instructions(bbgraph)
---     local bbgraph = bbgraph or self:bbgraph()
---     local all_instructions = {}
---     local auxmap = {}
-
---     -- getting all instructions
---     local i = 1
---     for _, bb in ipairs(bbgraph) do
---         for _, inst in ipairs(bb.ref:instructions()) do
---             all_instructions[inst] = {
---                 id = i,
---                 bb = bb,
---                 ref = inst,
---                 usages = {}
---             }
---             auxmap[inst:pointer()] = all_instructions[inst]
---             i = i + 1
---         end
---     end
-
---     -- mapping usages.
---     -- where the result of the instruction is used as an argument
---     for _, inst in pairs(all_instructions) do
---         for _, u in ipairs(inst.ref:usages()) do
---             local usage = auxmap[u]
---             table.insert(inst.usages, usage)
---         end
---     end
-
---     return all_instructions
--- end
-
--- -- puts the IR in true SSA form (withot useless alloca/store/load instructions)
--- function fn:ssa(bbgraph)
---     local bbgraph = bbgraph or self:bbgraph()
---     -- TODO Complete this function
-
---     local all_instructions = self:map_instructions(bbgraph)
---     for _, inst in pairs(all_instructions) do
---         if inst.ref:is_alloca() then
---             print("-> ", inst.ref, inst.ref:is_alloca())
---             for k, usage in pairs(inst.usages) do
---                 print('-->', usage.ref, usage.ref:is_store())
---             end
---         end
---     end
-
---     return bbgraph
--- end
 
 return fn
--- tests/test_functions.lua
+++ tests/test_functions.lua
@@ -43,137 +43,24 @@ do -- bbgraph
     assert(bbgraph)
     assert(type(bbgraph) == "table")
     assert(#bbgraph == 8)
-
-    local bb = bbgraphmap(bbgraph)
-
-    -- entry
-    assert(bbgraph[1] == bb.entry)
-    assert(bb.entry.predecessors:is_empty())
-    assert(bb.entry.successors == set.new(bb.b1))
-
-    -- b1
-    assert(bb.b1.predecessors == set.new(bb.entry))
-    assert(bb.b1.successors == set.new(bb.b2, bb.b3))
-
-    -- b2
-    assert(bb.b2.predecessors == set.new(bb.b1))
-    assert(bb.b2.successors == set.new(bb.b4, bb.b5))
-
-    -- b3
-    assert(bb.b3.predecessors == set.new(bb.b1))
-    assert(bb.b3.successors == set.new(bb.b5))
-
-    -- b4
-    assert(bb.b4.predecessors == set.new(bb.b2))
-    assert(bb.b4.successors == set.new(bb.exit))
-
-    -- b5
-    assert(bb.b5.predecessors == set.new(bb.b2, bb.b3))
-    assert(bb.b5.successors == set.new(bb.b6))
-
-    -- b6
-    assert(bb.b6.predecessors == set.new(bb.b5))
-    assert(bb.b6.successors == set.new(bb.exit))
-
-    -- exit
-    assert(bb.exit.predecessors == set.new(bb.b4, bb.b6))
-    assert(bb.exit.successors:is_empty())
+    -- complete test in test_bbgraph.lua
 end
 
-do -- domtree
+-- do -- map_instructions
+--     local instructions = main:map_instructions()
+--     for k, v in pairs(instructions) do
+--         print(k, v)
+--     end
+--     print(instructions)
+-- end
+
+do -- prunedssa
     local bbgraph = main:bbgraph()
-    local dom = main:domtree(bbgraph)
-    local bb = bbgraphmap(bbgraph)
+    local phis = main:prunedssa(bbgraph)
 
-    assert(dom[bb.entry] == set.new(bb.entry))
-    assert(dom[bb.b1] == set.new(bb.entry, bb.b1))
-    assert(dom[bb.b2] == set.new(bb.entry, bb.b1, bb.b2))
-    assert(dom[bb.b3] == set.new(bb.entry, bb.b1, bb.b3))
-    assert(dom[bb.b4] == set.new(bb.entry, bb.b1, bb.b2, bb.b4))
-    assert(dom[bb.b5] == set.new(bb.entry, bb.b1, bb.b5))
-    assert(dom[bb.b6] == set.new(bb.entry, bb.b1, bb.b5, bb.b6))
-    assert(dom[bb.exit] == set.new(bb.entry, bb.b1, bb.exit))
-end
-
-do -- sdomtree
-    local bbgraph = main:bbgraph()
-    local sdom = main:sdomtree(bbgraph)
-    local bb = bbgraphmap(bbgraph)
-
-    assert(sdom[bb.entry]:is_empty())
-    assert(sdom[bb.b1] == set.new(bb.entry))
-    assert(sdom[bb.b2] == set.new(bb.entry, bb.b1))
-    assert(sdom[bb.b3] == set.new(bb.entry, bb.b1))
-    assert(sdom[bb.b4] == set.new(bb.entry, bb.b1, bb.b2))
-    assert(sdom[bb.b5] == set.new(bb.entry, bb.b1))
-    assert(sdom[bb.b6] == set.new(bb.entry, bb.b1, bb.b5))
-    assert(sdom[bb.exit] == set.new(bb.entry, bb.b1))
-end
-
-do -- idomtree
-    local bbgraph = main:bbgraph()
-    local idom = main:idomtree(bbgraph)
-    local bb = bbgraphmap(bbgraph)
-
-    assert(idom[bb.entry] == nil)
-    assert(idom[bb.b1] == bb.entry)
-    assert(idom[bb.b2] == bb.b1)
-    assert(idom[bb.b3] == bb.b1)
-    assert(idom[bb.b4] == bb.b2)
-    assert(idom[bb.b5] == bb.b1)
-    assert(idom[bb.b6] == bb.b5)
-    assert(idom[bb.exit] == bb.b1)
-end
-
-do -- ridomtree
-    local bbgraph = main:bbgraph()
-    local ridom = main:ridomtree(bbgraph)
-    local bb = bbgraphmap(bbgraph)
-
-    assert(ridom[bb.entry] == set.new(bb.b1))
-    assert(ridom[bb.b1] == set.new(bb.b2, bb.b3, bb.b5, bb.exit))
-    assert(ridom[bb.b2] == set.new(bb.b4))
-    assert(ridom[bb.b3]:is_empty())
-    assert(ridom[bb.b4]:is_empty())
-    assert(ridom[bb.b5] == set.new(bb.b6))
-    assert(ridom[bb.b6]:is_empty())
-    assert(ridom[bb.exit]:is_empty())
-end
-
-do -- df
-    local bbgraph = main:bbgraph()
-    local df = main:df(bbgraph)
-    local bb = bbgraphmap(bbgraph)
-
-    assert(df[bb.entry]:is_empty())
-    assert(df[bb.b1]:is_empty())
-    assert(df[bb.b2] == set.new(bb.b5, bb.exit))
-    assert(df[bb.b3] == set.new(bb.b5))
-    assert(df[bb.b4] == set.new(bb.exit))
-    assert(df[bb.b5] == set.new(bb.exit))
-    assert(df[bb.b6] == set.new(bb.exit))
-    assert(df[bb.exit]:is_empty())
-end
-
-do -- dfplus
-    local bbgraph = main:bbgraph()
-    local bb = bbgraphmap(bbgraph)
-
-    -- a
-    local dfp_a = main:dfplus(bbgraph, set.new(bb.entry))
-    assert(dfp_a:is_empty())
-
-    -- x
-    local dfp_x = main:dfplus(bbgraph, set.new(bb.b2, bb.b3))
-    assert(dfp_x == set.new(bb.b5, bb.exit))
-
-    -- y
-    local dfp_y = main:dfplus(bbgraph, set.new(bb.b4))
-    assert(dfp_y == set.new(bb.exit))
-
-    -- z
-    local dfp_z = main:dfplus(bbgraph, set.new(bb.b1, bb.b5, bb.b6))
-    assert(dfp_z == set.new(bb.exit))
+    for instruction, phis in pairs(phis) do
+        print(instruction.ref:label(), phis)
+    end
 end
 
 testing.ok()
