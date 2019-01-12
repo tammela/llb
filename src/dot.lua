8344f0c5ce7826879532abd2c28e142d9ce9048f
Pedro Tammela <pctammela@gmail.com>
2019-01-12 12:51:00 -0200

    rename dot variable to file variable

--- src/dot.lua
+++ src/dot.lua
@@ -21,59 +21,59 @@
 local dot = {}
 
 function dot:bbgraph(bbgraph, name)
-    local dot = {}
-    table.insert(dot, 'digraph "CFG for ' .. name .. ' function" {')
-    table.insert(dot, '\tlabel="CFG for ' .. name .. ' function";\n')
+    local file = {}
+    table.insert(file, 'digraph "CFG for ' .. name .. ' function" {')
+    table.insert(file, '\tlabel="CFG for ' .. name .. ' function";\n')
     for _, node in ipairs(bbgraph) do
-        table.insert(dot, '\tNode' .. tostring(node.ref)
+        table.insert(file, '\tNode' .. tostring(node.ref)
             .. ' [shape=record,label="{' .. tostring(node.ref) .. '}"];')
         for s in pairs(node.successors) do
-            table.insert(dot, '\tNode' .. tostring(node.ref)
+            table.insert(file, '\tNode' .. tostring(node.ref)
                 .. ' -> ' .. 'Node' .. tostring(s.ref) .. ';')
         end
     end
-    table.insert(dot, '}')
-    return table.concat(dot, '\n')
+    table.insert(file, '}')
+    return table.concat(file, '\n')
 end
 
 function dot:domgraph(dom, name)
-    local dot = {}
-    table.insert(dot, 'digraph "Dominance graph for ' .. name
+    local file = {}
+    table.insert(file, 'digraph "Dominance graph for ' .. name
         .. ' function" {')
-    table.insert(dot, '\tlabel="Dominance graph for ' .. name
+    table.insert(file, '\tlabel="Dominance graph for ' .. name
         .. ' function";\n')
     for node, dominated in pairs(dom) do
-        table.insert(dot, '\tNode' .. tostring(node.ref)
+        table.insert(file, '\tNode' .. tostring(node.ref)
             .. ' [shape=record,label="{' .. tostring(node.ref) .. '}"];')
         for d in pairs(dominated) do
             if node.ref ~= d.ref then
-                table.insert(dot, '\tNode' .. tostring(d.ref)
+                table.insert(file, '\tNode' .. tostring(d.ref)
                     .. ' -> ' .. 'Node' .. tostring(node.ref) .. ';')
             end
         end
     end
-    table.insert(dot, '}')
-    return table.concat(dot, '\n')
+    table.insert(file, '}')
+    return table.concat(file, '\n')
 end
 
 function dot:idomgraph(idom, name)
-    local dot = {}
-    table.insert(dot, 'digraph "Imediate Dominance graph for ' .. name
+    local file = {}
+    table.insert(file, 'digraph "Imediate Dominance graph for ' .. name
         .. ' function" {')
-    table.insert(dot, '\tlabel="Imediate Dominance graph for ' .. name
+    table.insert(file, '\tlabel="Imediate Dominance graph for ' .. name
         .. ' function";\n')
     for node, dominated in pairs(idom) do
-        table.insert(dot, '\tNode' .. tostring(node.ref)
+        table.insert(file, '\tNode' .. tostring(node.ref)
             .. ' [shape=record,label="{' .. tostring(node.ref) .. '}"];')
         for d in pairs(dominated) do
             if node.ref ~= d.ref then
-                table.insert(dot, '\tNode' .. tostring(d.ref)
+                table.insert(file, '\tNode' .. tostring(d.ref)
                     .. ' -> ' .. 'Node' .. tostring(node.ref) .. ';')
             end
         end
     end
-    table.insert(dot, '}')
-    return table.concat(dot, '\n')
+    table.insert(file, '}')
+    return table.concat(file, '\n')
 end
 
 return dot
